3b4944fe8fd02d8a2b0865a0d131a2ca625554f0
efrat <E0533141835@gmail.com>
2026-04-23 17:15:37 +0300

    feat(types): define employee and manager summary interfaces

---
+++ src/models/summary.model.ts
@@ -0,0 +1,7 @@
+export interface IManagerSummary {
+  employee_name: string;      
+  strengths: string[];         
+  concerns_or_gaps: string[];  
+  manager_tip: string;        
+  createdAt?: Date;          
+}
